1508048778c3eef5a9f33985a8d34ad4cfd487c7
Tye Stanley <tyestanley090@gmail.com>
2023-06-26 10:05:59 -0700

    added hero image section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,7 +20,14 @@ const Hero = () => {
 
         <div className="hero__image-container">
           <div className="hero__image">
-            <Image src="" />
+            <Image
+              src="/hero.png"
+              alt="hero"
+              fill
+              className="object-contain"
+            />
+
+            <div className="hero__image-overlay" />
           </div>
         </div>
       </div>
