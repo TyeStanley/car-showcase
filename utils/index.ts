ceb1ebcdc7c4da1388b3f5a676db1c76061cdf90
Tye Stanley <tyestanley090@gmail.com>
2023-06-29 10:18:43 -0700

    updated fetchCars to use searchParams for user input on what type of car to search for

--- utils/index.ts
+++ utils/index.ts
@@ -1,14 +1,19 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     "X-RapidAPI-Key": "19ed93a3ddmsh3f7ad36a62a380ep138e1fjsne9e0d618f2cc",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com"
   };
 
-  const response = await fetch("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3", {
-    headers: headers
-  });
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers
+    }
+  );
 
   const result = await response.json();
 
